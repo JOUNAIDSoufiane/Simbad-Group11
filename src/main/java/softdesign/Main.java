38b162d3671093516d7a6728fb674cda72e43bd8
Unknown <soufianejounaid@gmail.com>
2019-03-08 21:45:15 +0100

    working generated code

--- src/main/java/softdesign/Main.java
+++ src/main/java/softdesign/Main.java
@@ -15,14 +15,18 @@ public class Main {
 
     public static void main(String[] args) {
         // request antialising so that diagonal lines are not "stairy"
+    	
         System.setProperty("j3d.implicitAntialiasing", "true");
         
         // creation of the environment containing all obstacles and robots
         EnvironmentDescription environment = new Environment();
         
+        CentralStation CS = new CentralStation();
+        
+        
         // adding two robots
-        Robot robot1 = new Robot(new Vector3d(0, 0, 0), "Robot 1");
-        Robot robot2 = new Robot(new Vector3d(-2, 0, -2), "Robot 2");
+        Robot robot1 = new Robot(new Vector3d(0, 0, 0), "Robot 1",CS);
+        Robot robot2 = new Robot(new Vector3d(-2, 0, -2), "Robot 2",CS);
 
         // add the two robots to the environment
         environment.add(robot1);
--- src/main/java/softdesign/Robot.java
+++ src/main/java/softdesign/Robot.java
@@ -11,17 +11,57 @@ import simbad.sim.CameraSensor;
 import simbad.sim.RobotFactory;
 import simbad.sim.RangeSensorBelt; 
 
-public class Robot extends Agent {
-	
-	private CameraSensor camera;
-    private BufferedImage cameraImage;
-    private RangeSensorBelt sonars, bumper;
-    private String behavior_pattern = "search";
-    private Point3d coordinates = new Point3d();
+public class Robot extends Agent
+{
+	/**
+				 * 
+				 */
+				private static String name;
+	/**
+	 * 
+	 */
+				private Point3d position = new Point3d();
 
-    public Robot(Vector3d position, String name) {
-        super(position, name);
-        
+	/**
+				 * 
+				 */
+				private CameraSensor camera;
+
+	/**
+				 * 
+				 */
+				private RangeSensorBelt sonars;
+
+	/**
+				 * 
+				 */
+				private BufferedImage camera_image;
+
+	/**
+				 * 
+				 */
+				private RangeSensorBelt bumper;
+
+	/**
+				 * 
+				 */
+				private String behavior_pattern;
+				
+				private CentralStation CS;
+				
+
+	/**
+	 * 
+	 * @param position 
+	 * @param name 
+	 * @param central_station 
+	 */
+	public Robot(Vector3d position, String name, CentralStation central_station) {
+		
+		super(position,name);
+		
+		CS = central_station;
+		
         // Add bumpers
         bumper = RobotFactory.addBumperBeltSensor(this);
         // Add sonars
@@ -29,146 +69,150 @@ public class Robot extends Agent {
         //add camera
         camera = RobotFactory.addCameraSensor(this);
         // reserve space for image capture
-        cameraImage = camera.createCompatibleImage();
-    }
+        camera_image = camera.createCompatibleImage();
+	}
 
-    /** This method is called by the simulator engine on reset. */
+	/**
+	 * 
+	 * @return 
+	 */
+	public String get_name() {
+		return name;
+	}
+
     public void initBehavior() {
-        System.out.println("I exist and my name is " + this.name);
+        System.out.println("I exist and my name is " + this.get_name());
         behavior_pattern = "search";
     }
-
-    /** This method is call cyclically (20 times per second) by the simulator engine. */
-    public void performBehavior() {
-    	
-    	//rover will always keep a distance of at least 0.5m from any obstacle
-    	double range_of_sonars = 0.5;
-    	
-    	this.getCoords(coordinates);
-    	
-    	if(behavior_pattern == "search")
-    	{
-    		if(bumper.oneHasHit())
-    		{
-    			this.setTranslationalVelocity(-0.5);
-    		}
-    		
-    		//avoid in front
-    		else if(sonars.hasHit(0) && sonars.getMeasurement(0) < range_of_sonars)
-    		{
-    			if(this.foundCube() == true)
-    			{
-    				this.setTranslationalVelocity(0.3);
-        			this.setRotationalVelocity(0);
-        			behavior_pattern = "found";
-    			}
-    			else
-    			{
-    				this.setTranslationalVelocity(-0.5);
-        			this.setRotationalVelocity(0.8);
-    			}
-    			
-    		}
-    		
-    		//avoid front left
-    		else if(sonars.hasHit(1) && sonars.getMeasurement(1) < range_of_sonars)
-    		{
-    			if(this.foundCube() == true)
-    			{
-    				this.setTranslationalVelocity(0.3);
-        			this.setRotationalVelocity(0.5);
-        			behavior_pattern = "found";
-    			}
-    			else
-    			{
-    				this.setTranslationalVelocity(-0.3);
-        			this.setRotationalVelocity(-0.5);
-    			}
-    		}
-    		
-    		//avoid left
-    		else if(sonars.hasHit(2) && sonars.getMeasurement(2) < range_of_sonars)
-    		{
-    			if(this.foundCube() == true)
-    			{
-    				this.setTranslationalVelocity(0.3);
-        			this.setRotationalVelocity(0.8);
-        			behavior_pattern = "found";
-    			}
-    			else
-    			{
-    				this.setTranslationalVelocity(0.1);
-        			this.setRotationalVelocity(-0.5);
-    			}
-    			
-    		}
-    		
-    		//avoid front right
-    		else if(sonars.hasHit(7) && sonars.getMeasurement(7) < range_of_sonars)
-    		{
-    			if(this.foundCube() == true)
-    			{
-    				this.setTranslationalVelocity(0.3);
-        			this.setRotationalVelocity(-0.5);
-        			behavior_pattern = "found";
-    			}
-    			else
-    			{
-    				this.setTranslationalVelocity(-0.3);
-        			this.setRotationalVelocity(0.5);
-    			}
-    		}
-    		
-    		//avoid right
-    		else if(sonars.hasHit(6) && sonars.getMeasurement(6) < range_of_sonars)
-    		{
-    			if(this.foundCube() == true)
-    			{
-    				this.setTranslationalVelocity(0.3);
-        			this.setRotationalVelocity(-0.8);
-        			behavior_pattern = "found";
-    			}
-    			else
-    			{
-    				this.setTranslationalVelocity(0.1);
-        			this.setRotationalVelocity(0.5);
-    			}
-    		}
-    		
-        	else {
-        		// the robot's speed is always 0.5 m/s
-                this.setTranslationalVelocity(0.5);
-                this.setRotationalVelocity(0);
-        	}
-    	}
-    	
-    	//once the cube is found, rover stops
-    	if(behavior_pattern == "found")
-		{
-			this.setTranslationalVelocity(0);
-            this.setRotationalVelocity(0);
-		}
-    }
     
-    //returns true once a rover finds the red cube in the environment
-    public boolean foundCube()
-    {
-		camera.copyVisionImage(cameraImage);
-		int color = cameraImage.getRGB(cameraImage.getHeight()/2, cameraImage.getWidth()/2);
-		int blue = color & 0xff;
-		int green = (color & 0xff00) >> 8;
-		int red = (color & 0xff0000) >> 16;
+	/**
+	 * 
+	 * @param behavior_pattern 
+	 */  
+    
+	public void performBehavior() {
 		
-    	if(red > 250 && green < 50 && blue < 50) //these values are used to truly find red and not black
-		{
-			System.out.println("Picture taken " + red);
-			return true;
-		}
+		//rover will always keep a distance of at least 0.5m from any obstacle
+		double range_of_sonars = 0.5;
 		
-		else
+		this.getCoords(position);
+		
+		if(behavior_pattern == "search")
 		{
-			return false;
+			if(bumper.oneHasHit())
+			{
+				this.setTranslationalVelocity(-0.5);
+			}
+			
+			//avoid in front
+			else if(sonars.hasHit(0) && sonars.getMeasurement(0) < range_of_sonars)
+			{
+				if(this.foundCube() == true)
+				{
+					this.setTranslationalVelocity(0.3);
+	    			this.setRotationalVelocity(0);
+	    			behavior_pattern = "found";
+				}
+				else
+				{
+					this.setTranslationalVelocity(-0.5);
+	    			this.setRotationalVelocity(0.8);
+				}
+				
+			}
+			
+			//avoid front left
+			else if(sonars.hasHit(1) && sonars.getMeasurement(1) < range_of_sonars)
+			{
+				if(this.foundCube() == true)
+				{
+					this.setTranslationalVelocity(0.3);
+	    			this.setRotationalVelocity(0.5);
+	    			behavior_pattern = "found";
+				}
+				else
+				{
+					this.setTranslationalVelocity(-0.3);
+	    			this.setRotationalVelocity(-0.5);
+				}
+			}
+			
+			//avoid left
+			else if(sonars.hasHit(2) && sonars.getMeasurement(2) < range_of_sonars)
+			{
+				if(this.foundCube() == true)
+				{
+					this.setTranslationalVelocity(0.3);
+	    			this.setRotationalVelocity(0.8);
+	    			behavior_pattern = "found";
+				}
+				else
+				{
+					this.setTranslationalVelocity(0.1);
+	    			this.setRotationalVelocity(-0.5);
+				}
+				
+			}
+			
+			//avoid front right
+			else if(sonars.hasHit(7) && sonars.getMeasurement(7) < range_of_sonars)
+			{
+				if(this.foundCube() == true)
+				{
+					this.setTranslationalVelocity(0.3);
+	    			this.setRotationalVelocity(-0.5);
+	    			behavior_pattern = "found";
+				}
+				else
+				{
+					this.setTranslationalVelocity(-0.3);
+	    			this.setRotationalVelocity(0.5);
+				}
+			}
+			
+			//avoid right
+			else if(sonars.hasHit(6) && sonars.getMeasurement(6) < range_of_sonars)
+			{
+				if(this.foundCube() == true)
+				{
+					this.setTranslationalVelocity(0.3);
+	    			this.setRotationalVelocity(-0.8);
+	    			behavior_pattern = "found";
+				}
+				else
+				{
+					this.setTranslationalVelocity(0.1);
+	    			this.setRotationalVelocity(0.5);
+				}
+			}
+			
+	    	else {
+	    		// the robot's speed is always 0.5 m/s
+	            this.setTranslationalVelocity(0.5);
+	            this.setRotationalVelocity(0);
+	    	}
 		}
 	
-    }
+		
+		//once the cube is found, rover stops
+		if(behavior_pattern == "found")
+		{
+			this.setTranslationalVelocity(0);
+	        this.setRotationalVelocity(0);
+		}
 }
+	
+	/**
+	 * 
+	 * @return 
+	 */
+    public boolean foundCube()
+    {
+		camera.copyVisionImage(camera_image);
+		return CS.found_obstacle(position,camera_image);
+    }
+    
+	public String get_behavior() {
+		return behavior_pattern;
+	}
+};
